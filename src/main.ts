d570a226474edb3378e357aeae16b3cd81663c78
nke680 <nke680>
2022-01-19 06:37:53 +1000

    copy files from repo

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import * as core from '@actions/core'
-// import {wait} from './wait'
+import * as event from './event'
+import * as version from './version'
+
+// import * as version from './version'
 
 export async function run(): Promise<void> {
   try {
-    // const ms: string = core.getInput('milliseconds')
-    // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    // core.debug(new Date().toTimeString())
-    // await wait(parseInt(ms, 10))
-    // core.debug(new Date().toTimeString())
-
-    // core.setOutput('time', new Date().toTimeString())
+    const tag = event.getCreateTag()
+    if (tag && version.isSemVer(tag)) {
+      //todo
+    }
     core.setOutput('release-url', 'https://example.com')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
