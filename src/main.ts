531007571fda321905c1a13de893189144c44d7c
nke680 <nke680>
2022-01-19 06:39:55 +1000

    copy files from repo

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
 import * as core from '@actions/core'
 import * as event from './event'
 import * as version from './version'
-
-// import * as version from './version'
+import * as git from './git'
+import * as github from './github'
 
 export async function run(): Promise<void> {
   try {
-    const tag = event.getCreateTag()
+    const token = core.getInput('repo-token')
+
+    const tag = event.getCreatedTag()
+    var releaseUrl = ''
+
     if (tag && version.isSemVer(tag)) {
-      //todo
+      const changelog = await git.getChangesIntroducedByTag(tag)
+      releaseUrl = await github.createReleaseDraft(tag, token, changelog)
     }
-    core.setOutput('release-url', 'https://example.com')
+
+    core.setOutput('release-url', releaseUrl)
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    core.setFailed(error.message)
   }
 }
 
